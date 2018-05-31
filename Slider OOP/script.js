1764c474cbbe7ef52e51b79a8b2a9b5a0ed0fe7d
Alex <Alex.P79@mail.ru>
2018-05-31 21:57:59 +0800

    Slider OOP

--- Slider OOP/script.js
+++ Slider OOP/script.js
@@ -1,27 +1,50 @@
 window.onload = function () {
+    /*Слайдер 1*/
+    var images = document.querySelectorAll('.gallery-1 .photos img');
+    var slider = new Slider(images);
 
-    var images = document.querySelectorAll('#gallery .photos img');
-    var btn_prev = document.querySelector('#gallery .buttons .prev');
-    var btn_next = document.querySelector('#gallery .buttons .next');
-    var i = 0;
-
-    btn_prev.onclick = function () {
-        images[i].classList.remove('showed');
-        i--;
-        if (i < 0) {
-            i = images.length-1;
-        }
-        images[i].classList.add('showed');
+    document.querySelector('.gallery-1 .buttons .prev').onclick = function () {
+        slider.prev();
     };
 
-    btn_next.onclick = function () {
-        images[i].classList.remove('showed');
-        i++;
-        if (i > images.length-1) {
-            i = 0;
-        }
-        images[i].classList.add('showed');
+    document.querySelector('.gallery-1 .buttons .next').onclick = function () {
+        slider.next();
     };
+    /*Слайдер 2*/
+    var images2 = document.querySelectorAll('.gallery-2 .photos img');
+    var slider2 = new Slider(images2);
+
+    document.querySelector('.gallery-2 .buttons .prev').onclick = function () {
+        slider2.prev();
+    };
+
+    document.querySelector('.gallery-2 .buttons .next').onclick = function () {
+        slider2.next();
+    };
+
+    /*Класс Slider*/
+    function Slider(images) {
+        this.images = images;
+        var i = 0;
+
+        this.prev = function () {
+            this.images[i].classList.remove('showed');
+            i--;
+            if (i < 0) {
+                i = this.images.length - 1;
+            }
+            this.images[i].classList.add('showed');
+        };
+
+        this.next = function () {
+            this.images[i].classList.remove('showed');
+            i++;
+            if (i > this.images.length - 1) {
+                i = 0;
+            }
+            this.images[i].classList.add('showed');
+        };
+    }
 
 };
 
